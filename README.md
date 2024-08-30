4ae17c41f753892ba69bed54a89798ae43eb4a19
Dhanar Adi Dewandaru <dhanar.adi.dewandaru@gmail.com>
2024-08-31 04:23:44 +0700

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# wsl2-kernel-zswap
+
+WSL2 kernel with zwap.
+
+Tested on Ubuntu 22.04 with kernel 5.15.153.1-microsoft-standard-WSL2.
+
+# Building
+
+```
+bash build.sh
+```
---
+++ build.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+
+# Build a custom WSL2 kernel with zwap
+
+set -e
+set -o pipefail
+
+sudo apt install build-essential flex bison libssl-dev libelf-dev libncurses-dev autoconf libudev-dev libtool
+
+WSL2_KERNEL_VERSION="$(uname -r | grep -o '^[0-9\.]\+')"
+
+wget -c https://github.com/microsoft/WSL2-Linux-Kernel/archive/refs/tags/linux-msft-wsl-${WSL2_KERNEL_VERSION}.tar.gz
+tar xvf linux-msft-wsl-${WSL2_KERNEL_VERSION}.tar.gz
+
+cd "WSL2-Linux-Kernel-linux-msft-wsl-${WSL2_KERNEL_VERSION}"
+
+cp Microsoft/config-wsl .config           # Use WSL default kernel config as the base
+cat << EOF >> .config
+
+CONFIG_FRONTSWAP=y
+CONFIG_ZSWAP=y
+CONFIG_ZSWAP_COMPRESSOR_DEFAULT_LZO=y
+CONFIG_ZSWAP_COMPRESSOR_DEFAULT="lzo"
+CONFIG_ZSWAP_ZPOOL_DEFAULT_ZBUD=y
+CONFIG_ZSWAP_ZPOOL_DEFAULT="zbud"
+CONFIG_ZSWAP_DEFAULT_ON=y
+CONFIG_ZPOOL=y
+CONFIG_ZBUD=y
+CONFIG_CRYPTO_LZO=y
+EOF
+make olddefconfig
+
+make -j $(nproc)
+
+cat << EOF
+
+Next, copy "arch/x86/boot/bzImage" to "/mnt/c" and 
+add the following to your ".wslconfig".
+
+[wsl2]
+kernel=C:\\bzImage
+
+After that, restart your WSL2 instance by executing 
+"wsl --shutdown" and then reopening your WSL2 terminal.
+EOF
